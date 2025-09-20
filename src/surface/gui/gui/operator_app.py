1985112073e9ca1fa85e088b37d30a5f01946ca7
Kelsey VDB <kelseyvdb28@gmail.com>
2025-09-20 13:40:55 -0400

    Removed carp model tab and related widgets Closes issue #30

--- src/surface/gui/gui/operator_app.py
+++ src/surface/gui/gui/operator_app.py
@@ -7,7 +7,6 @@ from gui.widgets.flood_warning import FloodWarning
 from gui.widgets.heartbeat import HeartbeatWidget
 from gui.widgets.ip_widget import IPWidget
 from gui.widgets.logger import Logger
-from gui.widgets.tabs.carp_model_tab import CarpModelTab
 from gui.widgets.tabs.dna_tab import DNATab
 from gui.widgets.tabs.general_debug_tab import GeneralDebugTab
 from gui.widgets.tabs.photosphere_tab import PhotosphereTab
@@ -58,7 +57,6 @@ class OperatorApp(App):
         self.tabs.addTab(main_tab, 'Main')
         self.tabs.addTab(GeneralDebugTab(), 'General Debug')
         self.tabs.addTab(PhotosphereTab(), 'Photosphere')
-        self.tabs.addTab(CarpModelTab(), 'Carp Model')
         self.shipwreck_tab = ShipwreckTab()
         self.tabs.addTab(DNATab(), 'DNA Sample')
         self.tabs.addTab(self.shipwreck_tab, SHIPWRECK_TEXT)
