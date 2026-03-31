33bbc49fd684041373b12d90997509fc6beec2f8
Shannon Griswold <shannon.v.griswold@gmail.com>
2026-03-31 10:28:40 -0400

    Add initial ros node that publishes messages

---
+++ src/surface/status_indicator/setup.py
@@ -0,0 +1,32 @@
+from glob import glob
+import os
+from pathlib import Path
+
+from setuptools import find_packages, setup
+
+PACKAGE_NAME = 'status_indicator'
+
+setup(
+    name=PACKAGE_NAME,
+    version='0.0.0',
+    packages=find_packages(exclude=['test']),
+    data_files=[
+        ('share/ament_index/resource_index/packages', ['resource/' + PACKAGE_NAME]),
+        ('share/' + PACKAGE_NAME, ['package.xml']),
+        (
+            str(Path('share') / PACKAGE_NAME / 'launch'),
+            [str(path) for path in Path('launch').glob('*launch.[pxy][yma]*')],
+        ),
+        (os.path.join('share', PACKAGE_NAME, 'config'), glob('config/*.yaml'))
+    ],
+    install_requires=['setuptools'],
+    zip_safe=True,
+    maintainer='shannon',
+    maintainer_email='svg33@case.edu',
+    description='Status Indicator for the ROV',
+    license='Apache License 2.0',
+    tests_require=['pytest'],
+    entry_points={
+        'console_scripts': ['status_indicator = status_indicator.status_indicator:main'],
+    },
+)
