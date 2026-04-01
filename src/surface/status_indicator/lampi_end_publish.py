e5fbd2b974555fc3d81b77431b08aa7be57ba587
Shannon Griswold <shannon.v.griswold@gmail.com>
2026-03-31 22:39:50 -0400

    Fixed merge conflict

--- src/surface/status_indicator/lampi_end_publish.py
+++ src/surface/status_indicator/lampi_end_publish.py
@@ -4,10 +4,10 @@ from paho.mqtt import publish
 
 while True:
     payload = 'true'
-    publish.single('helloMqtt', payload, hostname='localhost')
-    #print(payload)
+    publish.single('rov/helloMqtt', payload, hostname='localhost')
+    print(payload)
     time.sleep(3)
     payload = 'false'
-    publish.single('helloMqtt', payload, hostname='localhost')
-    #print(payload)
+    publish.single('rov/helloMqtt', payload, hostname='localhost')
+    print(payload)
     time.sleep(3)
