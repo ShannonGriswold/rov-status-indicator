34bd2b47e3008b1a91d9d895334e21ae4f8b62b2
Mollie Ackerman <molliesa2@gmail.com>
2026-04-12 11:58:40 -0400

    input field added

--- src/surface/gui/gui/widgets/tabs/indicator.py
+++ src/surface/gui/gui/widgets/tabs/indicator.py
@@ -1,4 +1,4 @@
-from PyQt6.QtWidgets import  QHBoxLayout, QPushButton, QVBoxLayout, QWidget, QLabel
+from PyQt6.QtWidgets import  QHBoxLayout, QLineEdit, QPushButton, QVBoxLayout, QWidget, QLabel
 from rclpy.qos import qos_profile_system_default
 from PyQt6.QtCore import QUrl, pyqtSignal, pyqtSlot
 
@@ -12,8 +12,9 @@ class IndicatorTab(QWidget):
     signal = pyqtSignal(VehicleState)
     def __init__(self) -> None:
         super().__init__()
+        self.input = QLineEdit(self)
+        #print(self.input.maximumWidth())
         self.armed = False
-        
         self.signal.connect(self.refresh)
         GUINode().create_signal_subscription(VehicleState, TOPIC_VEHICLE_STATE, self.signal, qos_profile_default)
         self.publisher = GUINode().create_publisher(VehicleState, TOPIC_CHANGE_VEHICLE_STATE,
@@ -21,7 +22,9 @@ class IndicatorTab(QWidget):
     
 
         top_bar = QHBoxLayout()
+        under_button_bar = QHBoxLayout()
         button1 = QPushButton()
+        under_button_bar.addWidget(self.input)
         self.armed_label = QLabel('Disarmed')
         top_bar.addWidget(self.armed_label)
         button1.setText('ARM')
@@ -38,6 +41,7 @@ class IndicatorTab(QWidget):
 
         top_bar.addStretch(2)
         root_layout = QVBoxLayout()
+        root_layout.addLayout(under_button_bar)
         root_layout.addLayout(top_bar)
         root_layout.addStretch()
         self.setLayout(root_layout)
